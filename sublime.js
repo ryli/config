64e479726e5ac141761553316964d32d307d70c9
ELEVEN <lov.2014@gmail.com>
2014-10-25 16:25:54 +0800

    add sublime

---
+++ sublime.js
@@ -0,0 +1,42 @@
+{
+	"Highlight_modified_tabs": true,
+	"auto_complete": true,
+	"auto_complete_delay": 500,
+	"auto_complete_triggers":
+	[
+		{
+			"characters": "<>\"'-_qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP",
+			"selector": "text, source, meta, string, punctuation, constant"
+		}
+	],
+	"caret_extra_bottom": 0,
+	"caret_extra_top": 0,
+	"caret_extra_width": 1,
+	"caret_style": "phase",
+	"color_scheme": "Packages/Solarized Color Scheme/Solarized (dark).tmTheme",
+	"default_encoding": "UTF-8",
+	"default_line_ending": "unix",
+	"draw_minimap_border": true,
+	"ensure_newline_at_eof_on_save": true,
+	"faont_face": "Monaco",
+	"font_size": 14,
+	"gutter": true,
+	"highlight_line": true,
+	"ignored_packages":
+	[
+		"Color Highlighter",
+		"Vintageous",
+		"PlainTasks",
+		"Phpcs",
+		"Vintage",
+		"GoSublime"
+	],
+	"indent_subsequent_lines": true,
+	"margin": -8,
+	"tab_completion": false,
+	"theme": "Soda SolarizedDark.sublime-theme",
+	"trim_trailing_white_space_on_save": true,
+	"update_check": false,
+	"word_separators": "./\\()\"':,.;<>~!@#$%^&*|+=[]{}`~?",
+	"word_wrap": true
+}
